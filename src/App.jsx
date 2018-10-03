ad2f9cedf1379d9f68f45e73291015aab763d166
Sebastian Mantilla <sebastian.mantilla@gmail.com>
2018-10-03 21:40:20 +0000

    Messages are sent to the websocket server and rendered when they come back.

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,62 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-import MessageList from  './MessageList.jsx';
-import ChatBar from  './ChatBar.jsx';
-import NavBar from  './NavBar.jsx';
+import MessageList from './MessageList.jsx';
+import ChatBar from './ChatBar.jsx';
+import NavBar from './NavBar.jsx';
 import { generateRandomId } from './util';
 
 class App extends Component {
+
   constructor(props) {
-    super();
-    this.state = { 
-      loaded: false, 
+    super(props);
+    this.wsSocket = new WebSocket('ws://0.0.0.0:3001/');
+    this.state = {
+      loaded: false,
       data: {
-        currentUser: {name: 'Bob'}, // optional. if currentUser is not defined, it means the user is Anonymous
-        messages: [
-          {
-            id: 1,
-            username: 'Bob',
-            content: 'Has anyone seen my marbles?',
-          },
-          {
-            id: 2,
-            username: 'Anonymous',
-            content: 'No, I think you lost them. You lost your marbles Bob. You lost them for good.'
-          }
-        ]
+        currentUser: { name: 'Bob' }, // optional. if currentUser is not defined, it means the user is Anonymous
+        messages: []
       }
     };
     this.addNewMessage = this.addNewMessage.bind(this);
+    this.wsSocket.onmessage = (event) => {
+      console.log("we got a message", event.data);
+      const newMessage = JSON.parse(event.data);
+      const messages = this.state.data.messages.concat(newMessage);
+      // code to handle incoming message
+      let data = { ...this.state.data };
+      data.messages = messages;
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({ data });
+    }
   }
 
   addNewMessage(message) {
-    const newMessage = {
-      content: message,
-      username: this.state.data.currentUser.name,
-      id: generateRandomId()
-    };
-    console.log(newMessage);
-    const messages = this.state.data.messages.concat(newMessage);
-    let data = {...this.state.data};
-    data.messages  = messages;
-    // Update the state of the app component.
-    // Calling setState will trigger a call to render() in App and all child components.
-    this.setState({data});
+    this.wsSocket.send(JSON.stringify({ username: this.state.data.currentUser.name, content: message }));
+    // const messages = this.state.data.messages.concat(newMessage);
+    // let data = { ...this.state.data };
+    // data.messages = messages;
+    // // Update the state of the app component.
+    // // Calling setState will trigger a call to render() in App and all child components.
+    // this.setState({ data });
   }
 
   componentDidMount() {
     console.log('componentDidMount <App />');
+    this.wsSocket.onopen = function (event) {
+      console.log('Connected to WebSocket server...');
+      // this.wsSocket.send("Here's some text that the server is urgently awaiting!"); 
+    };
     setTimeout(() => {
       console.log('Simulating incoming message');
       // Add a new message to the list of messages in the data store
-      const newMessage = {id: 3, username: 'Michelle', content: 'Hello there!'};
+      const newMessage = { id: 3, username: 'Michelle', content: 'Hello there!' };
       const messages = this.state.data.messages.concat(newMessage);
-      let data = {...this.state.data};
-      data.messages  = messages;
+      let data = { ...this.state.data };
+      data.messages = messages;
       // Update the state of the app component.
       // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({data});
+      this.setState({ data });
     }, 3000);
   }
 
